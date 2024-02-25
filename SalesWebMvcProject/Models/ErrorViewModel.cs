19852b302b0c359b44c9178dfbd0027552c0c672
Joao Vitor <githubconta2021@gmail.com>
2024-02-25 16:12:51 -0300

    Description: Projeto Created

---
+++ SalesWebMvcProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWebMvcProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
