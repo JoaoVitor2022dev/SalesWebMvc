b86970d84ba40e79bdd5f417f9840135f1b1a6eb
Joao Vitor <githubconta2021@gmail.com>
2024-02-26 18:53:12 -0300

    Description: CRUD Scaffolding

---
+++ SalesWebMvcProject/Data/SalesWebMvcProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcProject.Models;
+
+namespace SalesWebMvcProject.Data
+{
+    public class SalesWebMvcProjectContext : DbContext
+    {
+        public SalesWebMvcProjectContext (DbContextOptions<SalesWebMvcProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcProject.Models.Department> Department { get; set; } = default!;
+    }
+}
