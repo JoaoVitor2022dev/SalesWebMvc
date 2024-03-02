e7d1e944377d7ae35ce6ce8ef6b60dabb3060085
Joao Vitor <githubconta2021@gmail.com>
2024-03-02 17:29:02 -0300

    Description: Delete seller

--- SalesWebMvcProject/Services/SellerService.cs
+++ SalesWebMvcProject/Services/SellerService.cs
@@ -17,12 +17,26 @@ namespace SalesWebMvcProject.Services
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList(); 
-        }
+        } 
 
         // insert data in database
         internal void Insert(Seller obj)
         {
             _context.Add(obj); 
+            _context.SaveChanges();
+        }
+
+        // get data with id 
+        public Seller FindById (int id) 
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id); 
+        }
+
+        // Deleting data in database 
+        public void Remove(int id) 
+        { 
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
